5063b08eb1ec0148214a87b45ce1fe8921abedd7
EshwareeTondilkar <eshwareetondilkar@gmail.com>
2024-08-24 17:40:52 +0530

    Hello World program

---
+++ PNQ05/src/java_practice/HelloWorld.java
@@ -0,0 +1,10 @@
+package java_practice;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Friends Welcome To TNS Training...");
+	}
+
+}
